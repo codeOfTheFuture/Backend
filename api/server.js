e8b413880a9a6d67a8d3d22bb758ff3cd1ec819e
Jeff Oliver <jeffoliver3384@gmail.com>
2019-10-17 21:50:19 -0400

    changed prodection in knexfile to use sqlite3

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const globalMiddleware = require('../config/globalMiddleware');
 const authRouter = require('../auth/auth-router');
+const jokesRouter = require('../jokes/jokesRouter');
 
 const server = express();
 
 globalMiddleware(server);
 
 server.use('/api/auth', authRouter);
+server.use('/api/jokes', jokesRouter);
 
 server.get('/', (req, res) => {
   res.send('Dad Jokes');
