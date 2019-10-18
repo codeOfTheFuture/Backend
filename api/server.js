be152424d841de14c0e9a7d05750f79ced885337
Jeff Oliver <jeffoliver3384@gmail.com>
2019-10-17 20:42:30 -0400

    added product canvas to readme.md

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const globalMiddleware = require('../config/globalMiddleware');
+const authRouter = require('../auth/auth-router');
 
 const server = express();
 
 globalMiddleware(server);
+
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
   res.send('Dad Jokes');
--- user/userModel.js
+++ user/userModel.js
@@ -3,16 +3,21 @@ const db = require('../data/dbConfig');
 module.exports = {
   add,
   findById,
+  findBy,
 };
 
-const add = async user => {
+async function add(user) {
   const [id] = await db('users').insert(user);
 
-  return this.findById(id);
-};
+  return findById(id);
+}
 
-const findById = id => {
+function findById(id) {
   return db('users')
     .where({ id })
     .first();
-};
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
